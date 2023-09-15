0cebef1588fb3c7cb41a5735e084e243ca20008e
ngjunsiang <45561895+ngjunsiang@users.noreply.github.com>
2023-09-15 08:19:59 +0000

    Separate out game text into module

---
+++ data/text.py
@@ -0,0 +1,171 @@
+WELCOME = [
+    'Welcome to Hogwarts School of Witchcraft and Wizardry',
+    "The Dark Lord Voldemort has taken over Hogwarts School and opened multiple interdimensional gates, bringing hoards of enemies into the school. Your job as the chosen one is to traverse the school in order to locate The Shrieking Shack and thwart Voldemort's evil plan to take over the world"
+]
+
+WELCOME_YES = "You boldly opened the front gates of the school and made your way into the first room"
+
+WELCOME_NO = "Due to your utter cowardice, voldemort continued gaining power, spreading his control and chaos all over the world, leading to the complete annihlation of the human race"
+
+WELCOME_OTHER = "Due to your indecision, voldemort continued gaining power, spreading his control and chaos all over the world, leading to the complete annihlation of the human race"
+
+BREAK_SPELL = "Congratulations, you placed the two Dectus Medallions together releasing trememndous amounts of energy, breaking the powerful spell on the door"
+
+DOOR_LOCKED = "You tried entering the The Shrieking Shack but the door was locked by a powerful spell"
+
+DOOR_LOCKED_HINT = "You probably need to find a special item to break the spell (remember to loot all the rooms)"
+
+OUT_OF_MANA = "You do not have enough mana to cast spells"
+
+OUT_OF_FLASKS - "You ran out of flasks"
+
+NO_ACCESSORIES = "You do not have any accessories to equip"
+
+NO_ARMOUR = "You do not have any armour to equip"
+
+NO_WEAPON = "You do not have any weapon to equip"
+
+YOU_DIED = """
+__   _______ _   _  ______ _____ ___________
+\ \ / /  _  | | | | |  _  \_   _|  ___|  _  \\
+ \ V /| | | | | | | | | | | | | | |__ | | | |
+  \ / | | | | | | | | | | | | | |  __|| | | |
+  | | \ \_/ / |_| | | |/ / _| |_| |___| |/ /
+  \_/  \___/ \___/  |___/  \___/\____/|___/ 
+""".strip()
+
+GOD_SLAIN = """
+ _____ ___________   _____ _       ___  _____ _   _ 
+|  __ \  _  |  _  \ /  ___| |     / _ \|_   _| \ | |
+| |  \/ | | | | | | \ `--.| |    / /_\ \ | | |  \| |
+| | __| | | | | | |  `--. \ |    |  _  | | | | . ` |
+| |_\ \ \_/ / |/ /  /\__/ / |____| | | |_| |_| |\  |
+ \____/\___/|___/   \____/\_____/\_| |_/\___/\_| \_/
+""".strip()
+
+MEOW_1 = """  
+  __  __  U _____ u U  ___ u             
+U|' \/ '|u\| ___"|/  \/"_ \/__        __ 
+\| |\/| |/ |  _|"    | | | |\"\      /"/ 
+ | |  | |  | |___.-,_| |_| |/\ \ /\ / /\ 
+ |_|  |_|  |_____|\_)-\___/U  \ V  V /  U
+<<,-,,-.   <<   >>     \\  .-,_\ /\ /_,-.
+ (./  \.) (__) (__)   (__)  \_)-'  '-(_/ """
+
+MEOW_2 = """                                
+   ____ ___  ___  ____ _      __
+  / __ `__ \/ _ \/ __ \ | /| / /
+ / / / / / /  __/ /_/ / |/ |/ / 
+/_/ /_/ /_/\___/\____/|__/|__/"""
+
+MEOW_3 = """
+ .----------------.  .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. |
+| | ____    ____ | || |  _________   | || |     ____     | || | _____  _____ | |
+| ||_   \  /   _|| || | |_   ___  |  | || |   .'    `.   | || ||_   _||_   _|| |
+| |  |   \/   |  | || |   | |_  \_|  | || |  /  .--.  \  | || |  | | /\ | |  | |
+| |  | |\  /| |  | || |   |  _|  _   | || |  | |    | |  | || |  | |/  \| |  | |
+| | _| |_\/_| |_ | || |  _| |___/ |  | || |  \  `--'  /  | || |  |   /\   |  | |
+| ||_____||_____|| || | |_________|  | || |   `.____.'   | || |  |__/  \__|  | |
+| |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------' """
+
+MEOW_4 = """                              
+                              
+ _ __ ___   ___  _____      __
+| '_ ` _ \ / _ \/ _ \ \ /\ / /
+| | | | | |  __/ (_) \ V  V / 
+|_| |_| |_|\___|\___/ \_/\_/  
+                              
+                              """
+
+MEOW_5 = """ 
+ _  _  ____  __   _  _ 
+( \/ )(  __)/  \ / )( \
+/ \/ \ ) _)(  O )\ /\ /
+\_)(_/(____)\__/ (_/\_)"""
+
+MEOW_6 = """                              
+ _ __ ___   ___  _____      __
+| '_ ` _ \ / _ \/ _ \ \ /\ / /
+| | | | | |  __/ (_) \ V  V / 
+|_| |_| |_|\___|\___/ \_/\_/  
+                              """
+
+MEOW_7 = """                                    
+                                    
+,--,--,--. ,---.  ,---. ,--.   ,--. 
+|        || .-. :| .-. ||  |.'.|  | 
+|  |  |  |\   --.' '-' '|   .'.   | 
+`--`--`--' `----' `---' '--'   '--' 
+                                    """
+
+MEOW_8 = """ 
+ __    __     ______     ______     __     __    
+/\ "-./  \   /\  ___\   /\  __ \   /\ \  _ \ \   
+\ \ \-./\ \  \ \  __\   \ \ \/\ \  \ \ \/ ".\ \  
+ \ \_\ \ \_\  \ \_____\  \ \_____\  \ \__/".~\_\ 
+  \/_/  \/_/   \/_____/   \/_____/   \/_/   \/_/ 
+                                                 """
+
+MEOW_9 = """                                        
+                                        
+ _ .--..--.  .---.   .--.   _   _   __  
+[ `.-. .-. |/ /__\\/ .'`\ \[ \ [ \ [  ] 
+ | | | | | || \__.,| \__. | \ \/\ \/ /  
+[___||__||__]'.__.' '.__.'   \__/\__/   
+                                        """
+
+MEOW_10 = """ 
+_      _____ ____  _     
+/ \__/|/  __//  _ \/ \  /|
+| |\/|||  \  | / \|| |  ||
+| |  |||  /_ | \_/|| |/\||
+\_/  \|\____\\____/\_/  \|
+                          """
+
+def hit_wall(direction: str) -> str:
+    action = {
+        "left": "walked to the left",
+        "right": "walked to the right",
+        "forward": "walked forward",
+        "back": "turned back",
+    }
+    return f"You {action['direction']} and smashed into a wall"
+
+def loot_no(name: str) -> str:
+    return f"You left {name} on the ground and allowed the resourceful rat to steal it"
+
+def loot_other(name: str) -> str:
+    return f"Your indecisiveness allowed the resourceful rat to steal the {name} when you weren't looking"
+
+def spell_fail(name: str) -> str:
+    return f"You tried to cast {name} but it blew up in your face"
+
+def flask_fail(name: str) -> str:
+    return f"You tried drinking {name} but nothing happened"
+
+def flask_succeed(name: str, effect: str) -> str:
+    return f"You drank {name} and gained {effect}"
+
+def out_of_item(name: str) -> str:
+    return f"You ran out of {name}"
+
+def equip_fail(name: str) -> str:
+    return f"You tried changing your {name} but nothing happened"
+
+def equip_nonexistent(name: str) -> str:
+    return f"You tried equipping {name} but realised you cant create things out of thin air"
+
+def equip_succeed(name: str) -> str:
+    return f"You equipped {name}"
+
+def do_not_have(name: str) -> str:
+    return f"You do not own any {name}"
+
+def action_fail(action: str) -> str:
+    return f"You do not have the physical and mental capability to {action}"
+
+def obtain_thing(name: str, type: str) -> str:
+    return f"You obtained {name}, a powerful {type}"
